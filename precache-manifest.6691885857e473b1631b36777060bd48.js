afe450a925891393f6edcfcf90f0628464a781d5
nermolaev <nermolaev@artlebedev.ru>
2020-02-05 22:52:26 +0300

    Updates

---
+++ precache-manifest.6691885857e473b1631b36777060bd48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7e0dc75e85b83aa46985364a7c6787",
+    "url": "/pwa-wrkt/index.html"
+  },
+  {
+    "revision": "7490f1953e89329e7afa",
+    "url": "/pwa-wrkt/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "f3f3c9a7771cb2425256",
+    "url": "/pwa-wrkt/static/js/2.cbc3c4a9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pwa-wrkt/static/js/2.cbc3c4a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7490f1953e89329e7afa",
+    "url": "/pwa-wrkt/static/js/main.a6729729.chunk.js"
+  },
+  {
+    "revision": "6449ca947ecfda1a7447",
+    "url": "/pwa-wrkt/static/js/runtime-main.cd2f2219.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/pwa-wrkt/static/media/logo.ee7cd8ed.svg"
+  }
+]);
