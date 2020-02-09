547c41ed4cf6f7cf95ecf2b5837049bbd4fcd2cb
nermolaev <nermolaev@artlebedev.ru>
2020-02-09 19:13:31 +0300

    add ExerciseList and Exercise pages

---
+++ src/components/pages/Exercise/Exercise.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import './Exercise.scss';
+
+
+const Exercise = () => (<section>Exercise</section>);
+
+export default Exercise;
---
+++ src/components/pages/ExerciseList/ExerciseList.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import './ExerciseList.scss';
+
+
+const ExerciseList = () => (<section>ExerciseList</section>);
+
+export default ExerciseList;
