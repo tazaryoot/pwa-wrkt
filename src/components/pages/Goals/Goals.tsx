0964e82dc4d17205c1c489b6c57f8f1749464df8
nermolaev <nermolaev@artlebedev.ru>
2020-02-24 21:16:50 +0300

    modify component

--- src/components/pages/Goals/Goals.tsx
+++ src/components/pages/Goals/Goals.tsx
@@ -4,32 +4,36 @@ import { Link } from 'react-router-dom';
 import * as ls from '../../../helpers/localstorage'
 
 
-const ADD_GOAL = 'ADD_GOAL';
-const GET_GOAL_LIST = 'GET_GOAL_LIST';
-
-const initialState: GoalItem[] = [];
-
-export type Action =
-  | { type: 'ADD_GOAL', payload: GoalItem[] }
-  | { type: 'GET_GOAL_LIST', payload: GoalItem[]}
-
 interface GoalItem {
   title: string;
   code: string;
 }
 
-const reducer = (state: GoalItem[], action: Action): GoalItem[] => {
+type Goals = Record<string, GoalItem>;
+
+export type Action =
+  | { type: 'ADD_GOAL', payload: Goals }
+  | { type: 'GET_GOAL_LIST', payload: Goals}
+
+
+const ADD_GOAL = 'ADD_GOAL';
+const GET_GOAL_LIST = 'GET_GOAL_LIST';
+
+const initialState: Goals = {};
+
+
+const reducer = (state: Goals, action: Action): Goals => {
   switch (action.type) {
     case GET_GOAL_LIST:
-      return ([
+      return ({
         ...state,
         ...action.payload,
-      ]);
+      });
     case ADD_GOAL:
-      return ([
+      return ({
         ...state,
         ...action.payload,
-      ]);
+      });
     default:
       return state;
   }
@@ -41,7 +45,7 @@ const Goals = () => {
 
   useEffect(() => {
     const key = 'main-list';
-    const list = ls.getFromLS<GoalItem[]>(key) || [];
+    const list = ls.getFromLS<Goals>(key) || {};
 
     dispatch({
       type: 'GET_GOAL_LIST',
@@ -50,18 +54,17 @@ const Goals = () => {
 
   }, []);
 
-  const clickHandler = () => {
-    console.log('>>>>>> qq:', 11);
-  };
+  const goalCodes = Object.keys(goalList);
 
   return (
     <>
       <ul className="unstyled-list">
         {
-          !!goalList && goalList.map((x: GoalItem) => {
+          !!goalCodes.length && goalCodes.map((x: string) => {
+            const { code, title }: GoalItem = goalList[x];
             return (
-              <li key={x.code}>
-                <Link to={`/goal/${x.code}`}>{x.title}</Link>
+              <li key={code}>
+                <Link to={`/goal/${code}`}>{title}</Link>
               </li>
             )
           })
