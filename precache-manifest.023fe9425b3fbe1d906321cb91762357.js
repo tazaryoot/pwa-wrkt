55ecc1780514f07d32b26ad3d82fea78b61bca9a
tazaryoot <tazaryoot09@gmail.com>
2020-09-27 14:57:17 +0000

    Updates

---
+++ precache-manifest.023fe9425b3fbe1d906321cb91762357.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5aa5ed78d75c00ad90bd99591649e2",
+    "url": "/pwa-wrkt/index.html"
+  },
+  {
+    "revision": "bafe06e346a166e4a768",
+    "url": "/pwa-wrkt/static/css/main.a351742c.chunk.css"
+  },
+  {
+    "revision": "2163f5ad4b221457b395",
+    "url": "/pwa-wrkt/static/js/2.754321d3.chunk.js"
+  },
+  {
+    "revision": "6524af321f8d3e987e59afab1834492e",
+    "url": "/pwa-wrkt/static/js/2.754321d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bafe06e346a166e4a768",
+    "url": "/pwa-wrkt/static/js/main.22cb1ee0.chunk.js"
+  },
+  {
+    "revision": "229d0dc1a7cef804e784",
+    "url": "/pwa-wrkt/static/js/runtime-main.08ad8f01.js"
+  }
+]);
